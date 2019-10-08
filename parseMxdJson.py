bcbf6059ee9315bf90d223f29e0cce0c4f42db66
m006703 <sakai.yuta@mayo.edu>
2019-10-08 09:46:36 -0500

    Added the ability to handle cnv variants and incorporated argparse to choose files

--- parseMxdJson.py
+++ parseMxdJson.py
@@ -1,46 +1,79 @@
 import json
+import argparse
+import os
 
-open_json = open("/Users/m006703/Illumina/Knowledge_Network/files/nmpan.json", "r")
-upload_file = open("/Users/m006703/Illumina/Knowledge_Network/files/KN_Upload3.csv", "w")
-template_file = open("/Users/m006703/Illumina/Knowledge_Network/files/KN_Upload_Template.csv", "r")
-'''
-# This portion was to deal with the first set of MXD query from Eric
 
-# Make the MXD download file a valid json file
-json_without_comments.write("[\n")
-for line in open_json:
-    if line.startswith("/*"):
-        continue
-    else:
-        line = line.replace("ObjectId(", "")
-        line = line.replace(")", "")
-        line = line.replace("ISODate(", "")
-        line = line.replace("NumberLong(", "")
-        json_without_comments.write(line)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--inputJson", dest="input_path", required=True,
+        help="Full path to the input MXD json file"
+    )
+    parser.add_argument(
+        "-t", "--templateFile", dest="template_path", required=True,
+        help="Full path to the template Knowledge Network upload csv"
+    )
+    parser.add_argument(
+        "-o", "--outFile", dest="output_path", required=True,
+        help="Full path to the output csv file"
+    )
 
-json_without_comments.write("\n]")
-json_without_comments.close()
+    args = parser.parse_args()
 
-json_without_comments = open("/Users/m006703/Illumina/Knowledge_Network/Modified_Curations.json", "r")
+    input_path = os.path.abspath(args.input_path)
+    template_path = os.path.abspath(args.template_path)
+    output_path = os.path.abspath(args.output_path)
 
-json_data = json.load(json_without_comments)
-'''
-# Open json file
-json_file = json.load(open_json)
+    open_json = open(input_path, "r")
+    template_file = open(template_path, "r")
+    upload_file = open(output_path, "w")
 
-# Go to the "results" section of the json file
-json_data = json_file["results"]
+    # Open json file
+    json_file = json.load(open_json)
 
-# Copy the header from the template file
-for line in template_file:
-    line = line.rstrip()
-    upload_file.write(line)
-    upload_file.write("\n")
+    # Go to the "results" section of the json file
+    json_data = json_file["results"]
 
-# Parse the json file
-for entry in range(0, len(json_data)):
+    # Copy the header from the template file
+    for line in template_file:
+        line = line.rstrip()
+        upload_file.write(line)
+        upload_file.write("\n")
+
+    # Parse the json file
+    for entry in range(0, len(json_data)):
+        mxd_data = extract_mxd_data(json_data, entry)
+
+        # Write the above information into the upload file
+        upload_file.write(",".join(mxd_data))
+        upload_file.write("\n")
+
+    open_json.close()
+    upload_file.close()
+    template_file.close()
+
+
+def extract_mxd_data(json_data, entry):
+    """
+    Extracts the MXD data and converts it to Knowledge Network uploadable format.
+    Used within the loop to iterate through all queries for the json file
+    :param json_data:
+    :param entry:
+    :return:
+    """
     # Create an empty list to store the below variables
-    mxd_data = []
+    mxd_data_for_entry = []
+    # Get the variant type
+    try:
+        coordinateType = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["coordinateType"]
+        # MXD seems to be inconsistent on naming this variantSubtype or some don't have any
+        if coordinateType == "simple":
+            mxd_variant_type = "snv"
+        elif coordinateType == "cnv":
+            mxd_variant_type = "cnv"
+    except TypeError:
+        coorfinateType = "snv"
+    print(coordinateType + " and " + mxd_variant_type)
 
     referenceId = json_data[entry]["id"]
     print(referenceId)
@@ -56,22 +89,34 @@ for entry in range(0, len(json_data)):
         # Can't pull comments with the latest example from Eric, disabling this for now
         associationCuratorSummary = ""
         # Escape any "," in the note
-        #associationCuratorSummary = "\"" + associationCuratorSummary + "\""
+        # associationCuratorSummary = "\"" + associationCuratorSummary + "\""
     except KeyError:
         associationCuratorSummary = ""
     except TypeError:
         associationCuratorSummary = ""
-    # If MXD data has something other than SNV, the following two variables needs to be changed
-    type = "Variant"
-    curationLevel = "Nucleotide"
+    # Type and curation data depends on the variant_type
+    if mxd_variant_type == "snv":
+        kn_variant_type = "Variant"
+        curationLevel = "Nucleotide"
+    elif mxd_variant_type == "cnv":
+        kn_variant_type = "SV"
+        curationLevel = "Position"
     mutationClass = "Not Specified"
     multipleBiomarker = ""
     vid = ""
     chromosome = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["reference"]
-    # If the variant is SV, it will also need the stop position (start:stop)
-    position = str(json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["start"])
-    ref = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["referenceBases"]
-    alt = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["alternateBases"]
+    # SNVs will only have the start position and also has ref and alt
+    if mxd_variant_type == "snv":
+        position = str(json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["start"])
+        ref = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["referenceBases"]
+        alt = json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["alternateBases"]
+    # For CNVs, it needs start:stop and the ref and alt will be empty
+    elif mxd_variant_type == "cnv":
+        start_position = str(json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["start"]["position"])
+        stop_position = str(json_data[entry]["transientVariantDetails"]["coordinates"]["hg19"]["end"]["position"])
+        position = (start_position + ":" + stop_position)
+        ref = ""
+        alt = ""
     aminoAcid = ""
     codon = ""
     exonNumber = ""
@@ -97,18 +142,15 @@ for entry in range(0, len(json_data)):
     organization = ""
     licenseType = ""
     openAccess = ""
-
-    mxd_data.extend((referenceId, pathogenicity, inheritanceMode, confidence, penetrance, mechanism,
-                     associationCuratorSummary, type, curationLevel, mutationClass, multipleBiomarker,
+    mxd_data_for_entry.extend((referenceId, pathogenicity, inheritanceMode, confidence, penetrance, mechanism,
+                     associationCuratorSummary, kn_variant_type, curationLevel, mutationClass, multipleBiomarker,
                      vid, chromosome, position, ref, alt, aminoAcid, codon, exonNumber, " ".join(geneSymbol),
                      transcriptId, multipleDiseases, diseaseName, diseaseOntologyGroup, diseaseOntologyGroupId,
                      evidenceType, supportType, evidenceStrength, criteria, evidenceCuratorSummary, evidenceCategory,
                      source, content, pubmedId, url, organization, licenseType, openAccess))
 
-    # Write the above information into the upload file
-    upload_file.write(",".join(mxd_data))
-    upload_file.write("\n")
+    return mxd_data_for_entry
 
-open_json.close()
-upload_file.close()
-template_file.close()
+
+if __name__ == "__main__":
+    main()
